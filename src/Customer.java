c9877f90fcc1a72ca43b0b28b40aad447300aaff
shreyanabolleddu <162064440+shreyanabolleddu@users.noreply.github.com>
2024-03-11 17:24:22 -0700

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,281 @@
-public class Customer {
+/*
+ * @author Shreyana Bolleddu
+ * CIS 22C Team 1 Final Project
+ */
+
+import java.util.LinkedList;
+//import order package/class, add later
+
+public class Customer extends User {
+	private String address;
+	private String city;
+	private String state;
+	private String zip; 
+	private LinkedList<Order> shippedOrders; 
+	private LinkedList<Order> unshippedOrders;
+	
+	/**CONSTRUCTORS*/
+	
+    /**
+     * Creates a new Customer when only email
+     * and password are known
+     * @param username the User username
+     * @param password the User password
+     * Assigns firstName to "first name unknown"
+     * Assigns lastName to "last name unknown"
+     */
+    public Customer(String username, String password) {
+    	super(username, password);
+    }
+    
+    /**
+     * Creates a new Customer with name, username, and password information
+     * @param username the User username
+     * @param password the User password
+     * @param firstName User first name
+     * @param lastName User last name
+     */
+    public Customer(String firstName, String lastName, String username, String password) {
+    	super(firstName, lastName, username, password);
+    }
+    
+    /**
+     * Creates a new Customer when all information is known
+     * @param username the User username
+     * @param password the User password
+     * @param firstName User first name
+     * @param lastName User last name
+     */
+    public Customer(String firstName, String lastName, String username, String password,
+    		String address, String city, String state, String zip, LinkedList<Order> shippedOrders, 
+    		LinkedList<Order> unshippedOrders) { 
+    	//constructor can be changed to accommodate ArrayList for orders depending on code
+    	super(firstName, lastName, username, password);
+    	this.address = address;
+    	this.city = city;
+    	this.state = state;
+    	this.zip = zip;
+    	this.shippedOrders = shippedOrders;
+    	this.unshippedOrders = unshippedOrders;
+    	
+    	//sample code if using ArrayList for Orders
+    	/*
+        for (int i = 0; i < al.size(); i++) {
+          shippedOrders.addLast(al.get(i));
+          unshippedOrders.addLast(al.get(i));
+        }
+        */
+    }
+	
+    /**ACCESSORS*/
+
+    /**
+     * Accesses the Customer address
+     * @return the address
+     */
+    public String getAddress() {
+       return address;
+    }
+    
+    /**
+     * Accesses the Customer city
+     * @return the city
+     */
+    public String getCity() {
+       return city;
+    }
+	
+    /**
+     * Accesses the Customer state
+     * @return the state
+     */
+    public String getState() {
+    	return state;
+    }
+    
+    /**
+     * Accesses the Customer zip
+     * @return the zip
+     */
+    public String getZip() {
+    	return zip;
+    }
+    
+    /**
+     * Accesses the Customer list of shipped orders
+     * @return the list of shipped orders
+     */
+    public LinkedList<Order> getShippedOrders() {
+    	return shippedOrders;
+    }
+    
+    /**
+     * Accesses the Customer list of unshipped orders
+     * @return the list of unshipped orders
+     */
+    public LinkedList<Order> getUnshippedOrders() {
+    	return unshippedOrders;
+    }
+    
+    /**
+     * Accesses whether any shipped orders exist for this customer.
+     * @return whether the customer currently has an shipped orders.
+     */
+    public boolean hasShippedOrders() {
+       return !shippedOrders.isEmpty();
+    }
+    
+    /**
+     * Accesses whether any unshipped orders exist for this customer.
+     * @return whether the customer currently has an unshipped orders.
+     */
+    public boolean hasUnshippedOrders() {
+       return !unshippedOrders.isEmpty();
+    }
+    
+    /**MUTATORS*/
+
+    /**
+     * Updates the Customer address
+     * @param address a new address
+     */
+    public void setAddress(String address) {
+       this.address = address;
+    }
+    
+    /**
+     * Updates the Customer city
+     * @param city a new city
+     */
+    public void setCity(String city) {
+       this.city = city;
+    }
+	
+    /**
+     * Updates the Customer state
+     * @param state the new state
+     */
+    public void setState(String state) {
+    	this.state = state;
+    }
+    
+    /**
+     * Updates the Customer zip
+     * @param the new zip
+     */
+    public void setZip(String zip) {
+    	this.zip = zip;
+    }
+    
+    /**
+     * Updates the Customer list of shipped orders
+     * @param shippedOrders the new list of shipped orders
+     */
+    public void setShippedOrders(LinkedList<Order> shippedOrders) {
+    	this.shippedOrders = shippedOrders;
+    }
+    
+    /**
+     * Updates the Customer list of unshipped orders
+     * @param unshippedOrders the new list of unshipped orders
+     */
+    public void setUnshippedOrders(LinkedList<Order> unshippedOrders) {
+    	this.unshippedOrders = unshippedOrders;
+    }
+    
+    //will update the below methods as needed, may need different methods depending
+    //customer interface
+    
+    public boolean addOrder() {
+    	//fill in here
+    	return false;
+    }
+    
+    public boolean addShippedOrder() {
+    	//fill in here
+    	return false;
+    }
+    
+    public boolean addUnshippedOrder() {
+    	//fill in here
+    	return false;
+    }
+    
+    /**ADDITIONAL OPERATIONS*/
+
+    /**
+     * Creates a String of customer information in the form
+     * Name: <firstName> <lastName>
+     * Username: <userName>
+     * Address: <address> 
+     * <city>, <state> <zip>
+     * Order History
+     * Shipped Orders: 
+     * <shippedOrders>
+     * Unshipped Orders: 
+     * <unshippedOrders>
+     */
+    @Override
+    public String toString() {
+    	StringBuilder sb = new StringBuilder();
+    	sb.append("Name: " + getFirstName() + " " + getLastName());
+    	sb.append("\nUsername: " + getUsername());
+    	sb.append("Address: " + address);
+    	sb.append("\n" + city + ", " + state + " " + zip);
+    	sb.append("\nOrder History");
+    	sb.append("\nShipped Orders\n" + shippedOrders.toString());
+    	sb.append("\nUnshipped Orders\n" + unshippedOrders.toString());
+        return sb.toString();
+    }
+
+    /**
+     * Prints out all the Customer shipped orders.
+     */
+    public void printShippedOrders() {
+       System.out.print(shippedOrders.toString());
+    }
+
+    /**
+     * Prints out all the Customer unshipped orders.
+     */
+    public void printUnshippedOrders() {
+        System.out.print(unshippedOrders.toString());
+    }
+
+    /**
+     * Compares this Customer to another Object for equality.
+     * @param obj another Object
+     * @return true if obj is a Customer and has a matching username and password
+     * to this Customer.
+     */
+    @Override
+    public boolean equals(Object obj) {
+       if (this == obj) {
+          return true;
+       }
+       else if (!(obj instanceof Customer)) {
+          return false;
+       }
+       else {
+          Customer customer = (Customer) obj;
+          return this.getUsername().equals(customer.getUsername()) 
+          && this.getPassword().equals(customer.getPassword());
+       }
+    }
+
+    /**
+     * Returns a consistent hash code for each Customer by summing
+     * the Unicode values of each character in the key.
+     * Key = username + password
+     * @return the hash code
+     */
+    @Override
+    public int hashCode() {
+       int sum = 0;
+       String key = getUsername() + getPassword(); 
+       for (int i = 0; i < key.length(); i++) {
+          sum += (int)key.charAt(i);
+       }
+       return sum;
+    }
 }
